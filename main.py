e107c7472d1b5c8610883ed81117a6f3f53639b9
İbrahim bakıcı <bakici4546@gmail.com>
2023-04-09 22:52:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,74 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi import FastAPI , Path, BackgroundTasks, HTTPException
+from typing import Optional
 from pydantic import BaseModel
-from extract import *
 import os
-
-
-SECRET = os.getenv("SECRET")
-
-#
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
-    secret: str
+students = {
+    1:{
+        "name":"john",
+        "age":17,
+        "year":"year 12"
+    },
+    2:{
+        "name":"ibo",
+        "age":17,
+        "year":"year 12"
+    }
+}
 
-@app.get("/")
+class Student(BaseModel):
+    name : str
+    age : int
+    year : str
 
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+class StudentUpdate(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
 
 
-@app.get("/homepage")
-async def demo_get():
-    driver=createDriver()
+@app.get("/get-student/{student_id}")
+def get_students(student_id : int = Path(description="The ID of student you want to view",gt=0,lt=54)):
+    return students[student_id]
 
-    homepage = getGoogleHomepage(driver)
-    driver.close()
-    return homepage
+@app.get("/get-by-name")
+def get_student(students_id:Optional[int]= None , name : Optional[str] = None):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    # if students[students_id] == students[students_id]:
+    #     return students[students_id]
+    return {"Data": "Not Found"}
 
-@app.post("/backgroundDemo")
-async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
-    
-    background_tasks.add_task(doBackgroundTask, inp)
-    return {"message": "Success, background task started"}
-    
+@app.post("/create-student/{student_id}")
+def create_student(student_id : int, student : Student):
+    if student_id in students:
+        return {"error":"Student exists"}
 
+    students[student_id] = student
+    return students[student_id]
+
+@app.put("/update-Student/{student_id}")
+def update_student(student_id : int, student:StudentUpdate):
+    if student_id not in students:
+        return {"Error":"Student does not exists"}
+    if student.name != None:
+        students[student_id].name= student.name
+
+    if student.age != None:
+        students[student_id].age = student.age
+
+    if student.year != None:
+        students[student_id].year = student.year
+
+    return students[student_id]
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id:int):
+    if student_id not in students:
+        return {"Error":"Does not exists"}
+
+    del students[student_id]
+    return {"messager":"deleted"}
 
